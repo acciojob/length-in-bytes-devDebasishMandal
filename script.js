6b8754ccb1f5bfe30a2213f883da8ae26f6ba303
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-02 22:57:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const byteSize = (str) => {
   // write your code here
+	var blob = new Blob([str], { type: 'text/plain;charset=utf-8' });
+
+  // Get the size of the Blob (in bytes)
+  var sizeInBytes = blob.size;
+
+  return sizeInBytes;
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
+
